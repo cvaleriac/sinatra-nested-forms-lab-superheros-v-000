a3de66886e4ddbe13c273f5206d5b653e1da7ce6
valeria campilongo <cvaleriac@hotmail.it>
2018-11-06 14:37:37 +0000

    automatically backed up by learn

--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,13 +1,16 @@
-class Superhero
-  attr_accessor :name, :power, :bio
-
+class SuperHero
+  attr_reader :name, :power, :bio
   @@all = []
 
   def initialize(params)
     @name = params[:name]
     @power = params[:power]
     @bio = params[:bio]
-    @@all << self
+    self.save
+  end
+
+  def save
+    self.class.all << self
   end
 
   def self.all
